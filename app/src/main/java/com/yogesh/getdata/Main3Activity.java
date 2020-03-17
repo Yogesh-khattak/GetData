38eb1c9b98518b2e6fb7acdca5776333215c25e9
cmpundhir <cmpundhir@gmail.com>
2020-03-17 15:47:14 +0530

    storage added2

--- app/src/main/java/com/yogesh/getdata/Main3Activity.java
+++ app/src/main/java/com/yogesh/getdata/Main3Activity.java
@@ -2,25 +2,126 @@ package com.yogesh.getdata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main3Activity extends AppCompatActivity {
-    Button btn;
-
+    CallbackManager callbackManager;
+    private static final String EMAIL = "email";
+    LoginButton loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-        btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+////TO generate 28 char Hash key just use below code directly , by you need to change your app apackage name with "your.package"
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.yogesh.getdata",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//            Log.d("KeyHash",e.getMessage());
+//        } catch (NoSuchAlgorithmException e) {
+//            Log.d("KeyHash",e.getMessage());
+//        }
+//
 
 
-            }
-        });
+
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+//        // Callback registration
+//        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//            @Override
+//            public void onSuccess(LoginResult loginResult) {
+//                // App code
+//            }
+//
+//            @Override
+//            public void onCancel() {
+//                // App code
+//            }
+//
+//            @Override
+//            public void onError(FacebookException exception) {
+//                // App code
+//            }
+//        });
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        Toast.makeText(Main3Activity.this, "Success", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                        Toast.makeText(Main3Activity.this, "Cancel", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                        Toast.makeText(Main3Activity.this, "Error : "+exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(this, "onActivityResult", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean isLoggedIn(){
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        return isLoggedIn;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(isLoggedIn()){
+            Intent intent = new Intent(Main3Activity.this,Main2Activity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 }
